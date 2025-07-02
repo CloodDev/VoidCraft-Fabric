a392a9c0c1d4dba515485df11aadba9b10c6e316
Clood <cloodowy@gmail.com>
2025-07-03 01:25:43 +0200

    gowno straszne ale dziala

--- src/main/java/clood/voidcraft/block/ModBlocks.java
+++ src/main/java/clood/voidcraft/block/ModBlocks.java
@@ -15,26 +15,29 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-  public static final Block DEEPSLATE_ARGON_ORE = register("deepslate_argon_ore", Block::new,
-      AbstractBlock.Settings.create().strength(4.0f));
-  public static final Block ARGON_CRYSTAL_BLOCK = register("argon_crystal_block", Block::new,
-      AbstractBlock.Settings.create().strength(5.0f));
+  public static final Block DEEPSLATE_ARGON_ORE = registerBlock("deepslate_argon_ore", Block::new,
+      AbstractBlock.Settings.create().strength(4.0f).requiresTool());
+  public static final Block ARGON_CRYSTAL_BLOCK = registerBlock("argon_crystal_block", Block::new,
+      AbstractBlock.Settings.create().strength(5.0f).requiresTool());
 
-  private static Block register(String path, Function<AbstractBlock.Settings, Block> factory,
+  private static Block registerBlock(String path, Function<AbstractBlock.Settings, Block> factory,
       AbstractBlock.Settings settings) {
     final Identifier identifier = Identifier.of(VoidCraft.MOD_ID, path);
     final RegistryKey<Block> registryKey = RegistryKey.of(RegistryKeys.BLOCK, identifier);
 
     final Block block = Blocks.register(registryKey, factory, settings);
     Items.register(block);
+
     return block;
   }
 
   public static void registerModBlocks() {
     VoidCraft.LOGGER.info("Registering Blocks for " + VoidCraft.MOD_ID);
 
+    // Add blocks to item group
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-      entries.add(ModBlocks.DEEPSLATE_ARGON_ORE);
+      entries.add(DEEPSLATE_ARGON_ORE);
+      entries.add(ARGON_CRYSTAL_BLOCK);
     });
   }
 }
