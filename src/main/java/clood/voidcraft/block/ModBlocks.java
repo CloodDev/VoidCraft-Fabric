71a98f49828a6f40bd3de5e64db8377bb83307f3
Clood <cloodowy@gmail.com>
2025-07-02 23:54:51 +0200

    pain

--- src/main/java/clood/voidcraft/block/ModBlocks.java
+++ src/main/java/clood/voidcraft/block/ModBlocks.java
@@ -9,12 +9,19 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-  public static final Block DEEPSLATE_ARGON_ORE = registerBlock("deepslate_argon_ore",
-      new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+  public static final Block DEEPSLATE_ARGON_ORE;
+
+  static {
+    DEEPSLATE_ARGON_ORE = registerBlock("deepslate_argon_ore",
+        new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)
+            .registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(VoidCraft.MOD_ID, "deepslate_argon_ore")))));
+  }
 
   private static Block registerBlock(String name, Block block) {
     registerBlockItem(name, block);
@@ -23,7 +30,8 @@ public class ModBlocks {
 
   private static void registerBlockItem(String name, Block block) {
     Registry.register(Registries.ITEM, Identifier.of(VoidCraft.MOD_ID, name),
-        new BlockItem(block, new Item.Settings()));
+        new BlockItem(block,
+            new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VoidCraft.MOD_ID, name)))));
   }
 
   public static void registerModBlocks() {
--- src/main/java/clood/voidcraft/items/ModItems.java
+++ src/main/java/clood/voidcraft/items/ModItems.java
@@ -6,13 +6,19 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item ARGON_CRYSTAL = registerItem("argon_crystal", new Item(new Item.Settings()));
+
+    public static final Item ARGON_CRYSTAL = registerItem("argon_crystal", new Item(new Item.Settings()
+            .registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(VoidCraft.MOD_ID, "argon_crystal")))));
 
     private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(VoidCraft.MOD_ID, name), item);
+        Identifier id = Identifier.of(VoidCraft.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        return Registry.register(Registries.ITEM, key, item);
     }
 
     public static void registerModItems() {
